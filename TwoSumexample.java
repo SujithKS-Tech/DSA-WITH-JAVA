1dec9d40ceda74be511183e86d60e2b1c1b74968
sujith K S <kadursujith@gmail.com>
2026-08-21 20:44:15 +0530

    added TwoSumexample

---
+++ TwoSumexample.java
@@ -0,0 +1,14 @@
+public class TwoSumexample{
+    public static void main(String[] args) {
+        int arr[]={3,2,4};
+        int target=6;
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]+arr[j]==target){
+                    System.out.print("Indices:"+i+","+j);
+                }
+            }
+        }
+        return;
+    }
+}
