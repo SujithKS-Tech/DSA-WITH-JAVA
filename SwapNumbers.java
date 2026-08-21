f28cb8bf59544e845b8ea4b7a77f35d4f495a8ff
sujith K S <kadursujith@gmail.com>
2026-08-21 20:17:41 +0530

    added TwoSum

--- SwapNumbers.java
+++ SwapNumbers.java
@@ -1,14 +1,11 @@
 public class SwapNumbers {
     public static void main(String[] args) {
-
-        int a = 10;
-        int b = 20;
-
-        a = a + b;
-        b = a - b;
-        a = a - b;
-
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        int a=20;
+        int b=10;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
     }
 }
--- prefixsum.java
+++ prefixsum.java
@@ -1,14 +1,14 @@
 public class prefixsum{
     public static void main(String[] args) {
-        int a[]={10,20,30,40,50};
-        int n=5;
-        int psum[]=new int[n];
-        psum[0] = a[0];
-    for(int i=1; i<n; i++){
-        psum[i]=psum[i-1] + a[i];
-        }   
-    for(int i=0; i<n; i++){
+      int a[]={10,20,30,40,50,60,70,80,90,100};
+      int n=10;
+      int psum[]=new int[n];
+      psum[0]=a[0];
+      for(int i=1; i<n; i++){
+        psum[i]=psum[i-1]+a[i];
+      }
+      for(int i=0; i<n; i++){
         System.out.print(psum[i]+" ");
+      }
     }
-}
-}
+      }
