853dd9a989bc1dd49cf51896c734cb677f5d3af6
sujith <suje7267@gmail.com>
2026-09-17 21:49:42 +0530

    added

---
+++ PatternDecreasOdd.java
@@ -0,0 +1,11 @@
+public class PatternDecreasOdd{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=n; i++){
+             for(int j=1; j<=(n-i)+(n-i)+1; j++){
+                System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+}
