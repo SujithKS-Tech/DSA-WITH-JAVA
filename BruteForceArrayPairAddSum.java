325dd52bc36cf4859df0c381f424b47bdd6bd53e
sujith <kadursujith@gmail.com>
2026-09-18 10:19:47 +0530

    added

---
+++ BruteForceArrayPairAddSum.java
@@ -0,0 +1,12 @@
+public class BruteForceArrayPairAddSum {
+      public static void main(String[] args) {
+      int i,j,n=5;
+      int arr[]={1,2,3,4,5};
+      for(i=0;i<=n-1;i++){
+        for(j=0;j<=n-1;j++){
+          System.out.println(arr[i]+"+"+arr[j]+ "=" + (arr[i] + arr[j]));
+        }
+        System.out.println();  
+        }
+            }
+}
